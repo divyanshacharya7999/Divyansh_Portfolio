5d547f72158606891cb6034fbdf0fb616a643027
Divyansh Acharya <divyansh.acharya@efficience.ai>
2026-01-14 11:39:07 +0530

    alignment fix and issues resolved

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +8,18 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('Portfolio');
+
+  openTab(tabName: string, event: Event) {
+    const tabLinks = document.getElementsByClassName('tab-links');
+    const tabContents = document.getElementsByClassName('tab-contents');
+
+    for (let tabLink of tabLinks) {
+      tabLink.classList.remove('active-link');
+    }
+    for (let tabContent of tabContents) {
+      tabContent.classList.remove('active-tab');
+    }
+    (event.currentTarget as HTMLElement).classList.add('active-link');
+    document.getElementById(tabName)?.classList.add('active-tab');
+  }
 }
